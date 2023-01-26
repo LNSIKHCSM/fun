6f76191544116b75f2c8e1e817dfb9e840024782
Madhav <123637433+LNSIKHCSM@users.noreply.github.com>
2023-01-26 17:41:32 +0530

    Add files via upload

---
+++ javap/my first proj/src/App.java
@@ -0,0 +1,12 @@
+public class App{
+    public static void main(String[] args ){
+        int i,b;
+        b=5;
+        for( i=0;i<b;i++){
+            System.out.println(i);
+            System.out.println(b);
+
+        }
+        
+    }
+}
---
+++ javap/neon.java
@@ -0,0 +1,10 @@
+
+public class neon
+{
+	public static void main(String[] args) {
+	    int x=-34;
+	    
+		System.out.println(-34>>>3);
+	}
+}
+ 
